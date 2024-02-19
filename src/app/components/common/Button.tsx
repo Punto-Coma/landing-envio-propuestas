4eab2d0e053f2d5f8d85f480138fc8805b959cf3
AdrielIg <adrielgomezignacio@gmail.com>
2024-02-19 20:53:26 -0300

    add scaling to the condition

--- src/app/components/common/Button.tsx
+++ src/app/components/common/Button.tsx
@@ -8,9 +8,10 @@ interface Props {
 
 const Button = ({ disabled, onClick}:Props) => {
 
-    const sendButtonClass = cn('py-2 mx-auto w-36 rounded-lg bg-[#5865f2] text-neutral-50 cursor-default font-bold text-md mt-4 z-50 transition-all md:h-[50px] md:mt-8 ease-out duration-400 hover:scale-110',{
+    const sendButtonClass = cn('py-2 mx-auto w-36 rounded-lg bg-[#5865f2] text-neutral-50 cursor-default font-bold text-md mt-4 z-50 transition-all md:h-[50px] md:mt-8 ease-out duration-400 ',{
         'opacity-40': disabled,
-        'cursor-pointer': !disabled
+        'cursor-pointer hover:scale-110': !disabled,
+
     })
 
     const handleClick = (e:React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
