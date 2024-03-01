bc2c4bccda187c5e41d4081836f2a64a17aeea73
Mati Gummá <maalguna@gmail.com>
2024-03-01 14:14:47 -0300

    process.env

--- src/components/home/login.tsx
+++ src/components/home/login.tsx
@@ -3,7 +3,7 @@ export function Login() {
     return (
         <div className="max-w-2xl mx-auto p-4">
             <a
-                href={`https://discord.com/api/oauth2/authorize?client_id=${NEXT_CLIENT_ID}&redirect_uri=${NEXT_PUBLIC_URL}/callback&response_type=code&scope=identify+email+guilds+guilds.join`}
+                href={`https://discord.com/api/oauth2/authorize?client_id=${process.env.NEXT_CLIENT_ID}&redirect_uri=${NEXT_PUBLIC_URL}/callback&response_type=code&scope=identify+email+guilds+guilds.join`}
                 className="flex justify-between btn btn-ghost items-center" rel="noreferrer"
             >
                 <svg width={71} height={55} viewBox="0 0 71 55" fill="none"
