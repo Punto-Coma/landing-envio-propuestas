57e5773313226a45679c0f3b4db9f08a5b9bf8eb
Mati Gummá <maalguna@gmail.com>
2024-03-01 14:32:39 -0300

    envs needed

--- src/components/home/login.tsx
+++ src/components/home/login.tsx
@@ -1,9 +1,16 @@
 export function Login() {
 
+    const client_id = process.env.NEXT_CLIENT_ID;
+
+    const url = process.env.NEXT_PUBLIC_URL;
+
+    if(!client_id || !url)
+        return <div className="max-w-2xl mx-auto p-4">.ENVs needed</div>
+
     return (
         <div className="max-w-2xl mx-auto p-4">
             <a
-                href={`https://discord.com/api/oauth2/authorize?client_id=${process.env.NEXT_CLIENT_ID}&redirect_uri=${process.env.NEXT_PUBLIC_URL}/callback&response_type=code&scope=identify+email+guilds+guilds.join`}
+                href={`https://discord.com/api/oauth2/authorize?client_id=${}&redirect_uri=${}/callback&response_type=code&scope=identify+email+guilds+guilds.join`}
                 className="flex justify-between btn btn-ghost items-center" rel="noreferrer"
             >
                 <svg width={71} height={55} viewBox="0 0 71 55" fill="none"
